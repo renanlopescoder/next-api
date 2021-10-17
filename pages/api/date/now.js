c37094b78080aa1bca3f29c72c61b888af0c825e
Renan Lopes <renanlopescoder@gmail.com>
2021-10-17 18:01:44 +0200

    Cache control api

--- pages/api/date/now.js
+++ pages/api/date/now.js
@@ -2,5 +2,7 @@ export default function now(req, res) {
   const date = new Date();
   const format = date.toGMTString();
 
+  res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
+
   res.json({ now: format });
 }
